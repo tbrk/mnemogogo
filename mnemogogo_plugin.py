421c1c82380e44ec3b325b13d50853e936fb8a7b
Timothy Bourke <tim@tbrk.org>
2012-06-30 21:09:29 +1000

    Bump version number.

--- mnemogogo_plugin.py
+++ mnemogogo_plugin.py
@@ -60,7 +60,7 @@ def tr(msg):
     return QCoreApplication.translate("Mnemogogo", msg)
 
 name = 'Mnemogogo'
-version = "2.0.0"
+version = "2.0.1"
 exported_tag_name = "Mnemogogo"
 description = tr("Making mnemosyne mobile") + " (v" + version + ")"
 
